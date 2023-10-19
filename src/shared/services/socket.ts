5c73b2fa13dd137c7a9820c2246a42b4023fe710
vinicius <viniciusvwh1441@gmail.com>
2023-10-19 01:26:42 -0300

    chore: websocket_url env

--- src/shared/services/socket.ts
+++ src/shared/services/socket.ts
@@ -7,11 +7,11 @@ let socket: Socket
 export const getSocket = async (): Promise<Socket> => {
   if (!socket) {
     const response = await api.get(`/token`)
-    socket = io('http://localhost:3333', {
+    socket = io(process.env.WEBSOCKET_URL ?? 'http://localhost:3333', {
       query: {
         token: response.data.token
       }
-    }) // process.env.WEBSOCKET_URL!,
+    })
     socket.on('connect', () => {
       console.log('Conectado ao servidor')
     })
